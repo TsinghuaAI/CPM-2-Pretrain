f5138a8e3e512622f7828e271b899a97fdbaf363
t1101675 <gu-yx17@mails.tsinghua.edu.cn>
2021-06-01 06:25:38 +0000

    modify scripts

--- src/scripts/pretrain_enc_dec.sh
+++ src/scripts/pretrain_enc_dec.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
+
 WORKING_DIR=/home/thu-plm/CPM-2/
 
+python ${WORKING_DIR}/src/libbase/setup.py install
+
 # Change for multinode config
-MP_SIZE=4
+MP_SIZE=8
 
 DATA_PATH="${WORKING_DIR}/pretrain_data/wudao_corpus"
 
@@ -16,9 +19,10 @@ DS_CONFIG="${WORKING_DIR}/src/configs/deepspeed/ds_cpm2.json"
 TOKENIZER_PATH="${WORKING_DIR}/bpe_cn_en"　# 中英混合词表
 # TOKENIZER_PATH="${WORKING_DIR}/bpe_cn" # 中文词表
 
-BATCH_SIZE=26
-LR=0.001
-TRAIN_ITER=200000
+BATCH_SIZE=1
+LR=0.0001
+TRAIN_ITER=12800000
+LR_ITER=12800000
 
 ENC_LEN=512
 DEC_LEN=256
@@ -37,10 +41,9 @@ OPTS+=" --train-iters ${TRAIN_ITER}"
 OPTS+=" --save ${SAVE_PATH}"
 OPTS+=" --log-file ${LOG_FILE}"
 # OPTS+=" --load ${CKPT_PATH}"
+OPTS+=" --gradient-accumulation-steps 64"
 OPTS+=" --data-path ${DATA_PATH}"
 OPTS+=" --data-impl mmap"
-OPTS+=" --lazy-loader"
-OPTS+=" --tokenizer-type GPT2BPETokenizer"
 OPTS+=" --split 949,50,1"
 OPTS+=" --distributed-backend nccl"
 OPTS+=" --lr ${LR}"
@@ -48,17 +51,13 @@ OPTS+=" --no-load-optim"
 OPTS+=" --lr-decay-style noam"
 OPTS+=" --weight-decay 1e-2"
 OPTS+=" --clip-grad 1.0"
-OPTS+=" --warmup 0.05"
+OPTS+=" --warmup 0.01"
 OPTS+=" --tokenizer-path ${TOKENIZER_PATH}"
-OPTS+=" --save-interval 4000"
-OPTS+=" --eval-interval 2000"
+OPTS+=" --save-interval 50000"
+OPTS+=" --eval-interval 50000"
 OPTS+=" --eval-iters 10"
 OPTS+=" --log-interval 100"
-OPTS+=" --checkpoint-activations"
-OPTS+=" --deepspeed-activation-checkpointing"
 OPTS+=" --fp16"
-OPTS+=" --deepspeed"
-OPTS+=" --deepspeed_config ${DS_CONFIG}"
 
 CMD="deepspeed --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER} --hostfile ${HOST_FILE} ${WORKING_DIR}/src/pretrain_enc_dec.py $@ ${OPTS}"
 
